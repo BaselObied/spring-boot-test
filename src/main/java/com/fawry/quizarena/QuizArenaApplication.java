f830df29fa93e296c538c3d85156504d27008b9b
basel.obied <basel.obied@fawry.com>
2026-09-19 19:55:04 +0300

    rename

--- src/main/java/com/fawry/quizarena/QuizArenaApplication.java
+++ src/main/java/com/fawry/quizarena/QuizArenaApplication.java
@@ -1,4 +1,4 @@
-package com.fawry.springboottest;
+package com.fawry.quizarena;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/fawry/quizarena/QuizArenaApplicationTests.java
+++ src/test/java/com/fawry/quizarena/QuizArenaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fawry.springboottest;
+package com.fawry.quizarena;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
