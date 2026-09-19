5d4f9dd3f7f9683f6c6036b18f92074024fbf9c5
basel.obied <basel.obied@fawry.com>
2026-09-19 19:54:20 +0300

    rename

--- src/main/java/com/fawry/springboottest/QuizArenaApplication.java
+++ src/main/java/com/fawry/springboottest/QuizArenaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringboottestApplication {
+public class QuizArenaApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(SpringboottestApplication.class, args);
+        SpringApplication.run(QuizArenaApplication.class, args);
     }
 
 }
