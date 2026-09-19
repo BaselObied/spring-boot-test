b7646e99b69acde2c821fda7f83eedf972d48d83
basel.obied <basel.obied@fawry.com>
2026-09-19 18:54:25 +0300

    init

--- src/main/java/com/fawry/springboottest/SpringboottestApplication.java
+++ src/main/java/com/fawry/springboottest/SpringboottestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringboottestApplication {
 
-    void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(SpringboottestApplication.class, args);
     }
 
